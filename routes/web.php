1cf3fa074c1d7185d2df4629123b1da05a27c514
Sandeep Dewangan <sandeepdewangan@192.168.1.7>
2023-12-23 08:47:45 +0530

    Spatie Role and Permission Setup

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
-Auth::routes();
+Auth::routes([
+    'register'=>false
+]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
