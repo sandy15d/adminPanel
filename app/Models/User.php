3c1f2b81b738520509e56c49c391c39abcdc1355
Sandeep Kumar Dewangan <sandeepdewangan2012@gmail.com>
2023-12-25 11:42:21 +0530

    Record User Login & LogOut Activity

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,12 +7,15 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Activitylog\LogOptions;
+
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
     use HasRoles;
+
 
     /**
      * The attributes that are mass assignable.
@@ -44,4 +47,6 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,10 +15,10 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Administrator',
-        //     'email' => 'admin@admin.com',
-        //     'password'=>Hash::make('password'),
-        // ]);
+         /*\App\Models\User::factory()->create([
+             'name' => 'Administrator',
+             'email' => 'admin@admin.com',
+             'password'=>Hash::make('password'),
+         ]);*/
     }
 }
